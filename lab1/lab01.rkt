f47e39140fa1450366bd41aca3837ca171fbbcf6
JohnHenry <johnhenry514@gmail.com>
2019-07-03 13:07:40 -0700

    3/4 done

--- lab1/lab01.rkt
+++ lab1/lab01.rkt
@@ -8,11 +8,18 @@
 (define sum
   (lambda (lst)
     (if (null? lst)
-        lst
+        0
         (+ (car lst) (sum (cdr lst))))))
 
+(define retrieve-first-n
+  (lambda (n lst)
+    (cond ((< n 1) '())
+          ((> n (length lst)) '())
+          (else (cons (car lst) (retrieve-first-n (- n 1) (cdr lst)))))))
 
           
-
+(define pair-sum?
+  (lambda (lst n)
+    (if (= (+ (car lst)
        
     
