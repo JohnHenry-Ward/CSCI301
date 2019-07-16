852dfadebf00c7ea260fe9827ff9bb80f32503e4
JohnHenry <johnhenry514@gmail.com>
2019-07-15 17:19:33 -0700

    finished

--- lab3/lab03.rkt
+++ lab3/lab03.rkt
@@ -1,53 +1,29 @@
 #lang racket
+;;-----helper functions-----;;
 
+;;function checks if a single element is in a list L
 (define member?
   (lambda (x L)
     (if (null? L) 
         #f
         (or (equal? x (car L)) (member? x (cdr L)))))) ;;first checks if 1st pos = x, then recursive call on cdr of list
 
-(define subset?
-  (lambda (L1 L2)
-    (if (null? L1) ;;if L1 is empty, then it is a subset (empty set is always a subset)
-        #t
-        (if (member? (car L1) L2)
-            (subset? (cdr L1) L2)
-            #f))))
-
-(define 1-set-equal?
-  (lambda (L1 L2)
-    (and (equal? (length L1) (length L2)) (subset? L1 L2)))) ;;if L1 and L2 are same length, and L1 is subset of L2
-
+;;function checks if a single set L1 a set within the set L2
+;;L1 does not contain other sets, only single elements
 (define set-exists?
   (lambda (L1 L2)
-    ;;L1 is 1 set containing no other sets
-    ;;L2 is 1 set that could contain other sets
-    ;;check if (null? L1)
-        ;;false - check if (car L2) is a list
-            ;;true - check if L1 subset (car L2)
-                ;;true - return true
-                ;;false - (set-exists? L1 (cdr L2))
-            ;;false - set-exsits? L1 (cdr L2))
-        ;;true - return true
     (if (null? L1) #t
         (if (null? L2) #f
             (if (list? (car L2))
-                (if (1-set-equal? L1 (car L2)) #t
+                (if (set-equal? L1 (car L2)) #t
                     (set-exists? L1 (cdr L2)))
                 (set-exists? L1 (cdr L2)))))))
+
+;;-----end helper functions-----;;
                 
-            
+;;function checks if two sets are equal to each other (sets may contain sets)         
 (define set-equal?
   (lambda (L1 L2)
-    ;;check if (null? L1)
-        ;;false - check if (car L1) is a list
-            ;;true - check if that list exists in L2
-                ;;true - set-equal? (cdr L1 L2)
-                ;;false - terminate
-            ;;false - check if that x is a member of L2
-                ;;true - set-equal? (cdr L1 L2)
-                ;;false - terminate
-        ;;true - return true
     (if (null? L1) #t
         (if (list? (car L1))
             (if (set-exists? (car L1) L2)
@@ -57,28 +33,37 @@
                 (set-equal? (cdr L1) L2)
                 #f)))))
 
+;;function returns a new set from all elements of two sets (ignoring duplicates)
 (define union
   (lambda (S1 S2)
-    ;;check if set-equal?
-        ;;true - S1
-        ;;false - check if (null? S1)
-            ;;true - S2
-            ;;false - check if (car S1) is a list
-                ;;true - check if set-equal? (car S1) S2
-                    ;;true - union (cdr S1) S2
-                    ;;false - cons (car S1) union (cdr S1) S2)
-                ;;false - check if (car S1) is a member of S2
-                    ;;true - union (cdr S1) S2
-                    ;;false - cons (car S1) (union (cdr S1) S2)
-    (if (set-equal? S1 S2) S1
+    (if (set-equal? S1 S2) S2
         (if (null? S1) S2
-            (if (list? (car S1))
+            ;;checks if its a list (longer than 1) or a single value
+            (if (and (list? (car S1)) (> (length (car S1)) 1))
+                ;;(car S1) is a list
                 (if (set-equal? (car S1) S2)
                     (union (cdr S1) S2)
                     (cons (car S1) (union (cdr S1) S2)))
+                ;;(car S1) is a single element, or a list with 1 item
                 (if (member? (car S1) S2)
                     (union (cdr S1) S2)
-                    (cons (car S1) (union (cdr S1) S2)))))))
+                    (cons (car S1) (union (cdr S1) S2))))))))
+
+;;function returns a new set from all elements that are in both sets (ignoring duplicates)
+(define intersect
+  (lambda (S1 S2)
+    (if (or (null? S1) (null? S2)) '()
+        (if (set-equal? S1 S2) S1
+            ;;checks if its a list (longer than 1) or a single value
+            (if (and (list? (car S1)) (> (length (car S1)) 1))
+                ;;(car S1) is a list
+                (if (set-equal? (car S1) S2)
+                    (cons (car S1) (intersect (cdr S1) S2))
+                    (intersect (cdr S1) S2))
+                ;;(car S1) is a single element, or a list with 1 item
+                (if (member? (car S1) S2)
+                    (cons (car S1) (intersect (cdr S1) S2))
+                    (intersect (cdr S1) S2)))))))
                     
                     
 
