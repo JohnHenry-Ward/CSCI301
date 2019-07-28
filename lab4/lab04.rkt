f36cf17056a87f0e64c90f1755b4d42888e86d85
JohnHenry <johnhenry514@gmail.com>
2019-07-27 18:52:43 -0700

    submitted

--- lab4/lab04.rkt
+++ lab4/lab04.rkt
@@ -1,5 +1,8 @@
 #lang racket
-;;reflexive?: xRx for every x
+;;reflexive? xRx
+;;first checks if the relation is valid over the set S
+;;if true, then it calls real-reflexive? (which is recursive)
+
 (define reflexive?
   (lambda (L S)
     (if (not (relation? L S)) #f
@@ -10,56 +13,62 @@
     (if (null? S) #t
         (if (< (length L) (length S)) #f
             (if (equal? (car (car L)) (car (cdr (car L))))
-                (real-reflexive? (cdr L) (remove (car (car L)) S))
+                (real-reflexive? (cdr L) (remove (car (car L)) S)) ;;removes first position in S to make progress
                 (real-reflexive? (cdr L) S))))))
+
 ;;-----end reflexive?-----;;        
 
-;;symmetric?: xRy => yRx
+;;symmetric? xRy => yRx
+;;i am assuming that their could be duplicate pairs, since it's not specified in the lab
+
 (define symmetric?
   (lambda (L)
     (if (null? L) #t
         (if (set-exists? (reverse (car L)) L)
             (if (equal? (car (car L)) (car (cdr (car L))))
-                (symmetric? (remove* (list (car L)) L))
-                (symmetric? (remove* (list (reverse (car L))) (remove* (list (car L)) L))))
+                (symmetric? (remove* (list (car L)) L)) ;;removes only the pair that is reflexive (a, a)
+                (symmetric? (remove* (list (reverse (car L))) (remove* (list (car L)) L)))) ;;removes both the initial pair and the reverse pair
             #f))))
+
 ;;-----end symmetric?-----;;
 
-;;transitive: xRy and yRz => xRz
+;;transitive? xRy and yRz => xRz
+;;transitive? calls real-transitive? so that I can have 2 lists to use
+
 (define transitive?
   (lambda (L)
     (real-transitive? L L)))
+
+;;real-transitive? takes 2 lists, one is always the inital list L, the other is cdr L so that progress can be made
 
 (define real-transitive?
   (lambda (L changeL)
     (if (null? changeL) #t
         (if (check-for-transitive L (car changeL) L)
-            (real-transitive? L (cdr changeL))
+            (real-transitive? L (cdr changeL)) ;;progress is made
             #f))))
+
+;;check for transitive takes 3 inputs (this is where most of the work is done)
+;;the inital list L, which is used to check if a new transitive pair is in L
+;;a pair that is used to check for a possible new transitive pair
+;;resetL which is used to go down L to check for possible transitive pair, and is 'reset' once all possible pairs have been found
 
 (define check-for-transitive
  (lambda (L pair resetL)
    (if (null? resetL) #t
-       (if (equal? (car (car resetL)) (car (cdr pair)))
-           (if (set-exists? (list (car pair) (car (cdr (car resetL)))) L)
-               (check-for-transitive L pair (cdr resetL))
+       (if (equal? (car (car resetL)) (car (cdr pair))) ;;if link is found (a b) (b c)
+           (if (set-exists? (list (car pair) (car (cdr (car resetL)))) L) ;;check if new pair (a c) is in L
+               (check-for-transitive L pair (cdr resetL)) ;;keep checking resetL for possible links
                #f)
            (check-for-transitive L pair (cdr resetL))))))
+
 ;;-----end transitive?-----;;
-        
         
 
 ;;-----Helper Functions-----;;
 
-;;function checks if their is a set (y z) in L when given pair (x y), and returns that pair (y z)
-(define link-found?
-  (lambda (L pair)
-    (if (null? L) '()
-        (if (equal? (car (cdr pair)) (car (car L)))
-            (car L)
-            (link-found? (cdr L) pair)))))
-
 ;;function checks if a set L is a relation of the set S
+
 (define relation?
   (lambda (L S)
     (if (null? L) #t
@@ -68,6 +77,7 @@
             #f))))
 
 ;;function checks if a single element is in a list L
+
 (define member?
   (lambda (x L)
     (if (null? L) 
@@ -76,6 +86,7 @@
      
 ;;function checks if a single set L1 is a set within the set L2
 ;;L1 does not contain other sets, only single elements
+
 (define set-exists?
   (lambda (L1 L2)
     (if (null? L1) #t
